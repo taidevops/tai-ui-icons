66584f29ed58effc1549ce94ccd63132a0c342cb
Phan Tấn Tài <phantantai9800@gmail.com>
2021-10-08 14:49:47 +0700

    tai

---
+++ packages/react-icons/src/utils/IFluentIconsProps.types.ts
@@ -0,0 +1,5 @@
+export interface IFluentIconsProps {
+	primaryFill?: string
+	className?: string
+	filled?: boolean
+}
---
+++ packages/react-icons/src/utils/wrapIcon.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { css } from "./css";
+import "./WrappedIcon.scss";
+import { IFluentIconsProps } from "./IFluentIconsProps.types";
+
+const wrapIcon = (icon: JSX.Element, displayName?: string) => {
+  const Component: React.FC<React.HTMLAttributes<HTMLSpanElement> & IFluentIconsProps> = ({ 
+    primaryFill = "currentColor", 
+    ...props 
+  }) => {
+    const { className } = props;
+    const containerProps = props["aria-label"] || props["aria-labelledby"] || props.title
+    ? {
+        role: "img",
+      }
+    : {
+        ["aria-hidden"]: true,
+      };
+
+    return (
+      <span {...props} {...containerProps} className={css("root-span", className)}>
+        {React.cloneElement(
+          icon, 
+          {
+            className: "svg",
+            fill: primaryFill,
+          }
+        )}
+      </span>
+    );
+  };
+  Component.displayName = displayName;
+  return Component;
+};
+
+export default wrapIcon;
